cab1b50210724c498d6333815439ae0eef9aeb8f
trieu <tantrieuf31.database@gmail.com>
2025-11-12 15:18:58 +0700

    fix bugs

--- resources/app-templates/leocdp-admin/common-resources-min/leocdp.onboarding.js
+++ resources/app-templates/leocdp-admin/common-resources-min/leocdp.onboarding.js
@@ -1,2 +1,2 @@
-// Version: v_0.9.0_2025-11-05-15-42-45
+// Version: v_0.9.0_2025-11-12-15-18-32
 'use strict';function initMainMenuGuide(){$('[data-toggle\x3d"popover"]').popover({delay:{show:500,hide:100},trigger:"hover"})};

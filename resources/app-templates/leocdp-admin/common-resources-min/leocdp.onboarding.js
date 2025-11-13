0b9810fa366e49220ff3f646762a3955224028bd
trieu <trieu@leocdp.com>
2025-11-14 01:35:01 +0700

    improve performance

--- resources/app-templates/leocdp-admin/common-resources-min/leocdp.onboarding.js
+++ resources/app-templates/leocdp-admin/common-resources-min/leocdp.onboarding.js
@@ -1,2 +1,2 @@
-// Version: v_0.9.0_2025-11-12-15-18-32
+// Version: v_0.9.0_2025-11-14-01-33-44
 'use strict';function initMainMenuGuide(){$('[data-toggle\x3d"popover"]').popover({delay:{show:500,hide:100},trigger:"hover"})};

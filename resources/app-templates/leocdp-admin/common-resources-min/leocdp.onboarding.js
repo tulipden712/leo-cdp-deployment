ae1a7a90b683ca1ea1671061b1ffa1b4e8946c20
trieu <tantrieuf31.database@gmail.com>
2025-11-17 13:54:37 +0700

    improve performance

--- resources/app-templates/leocdp-admin/common-resources-min/leocdp.onboarding.js
+++ resources/app-templates/leocdp-admin/common-resources-min/leocdp.onboarding.js
@@ -1,2 +1,2 @@
-// Version: v_0.9.0_2025-11-14-01-33-44
+// Version: v_0.9.0_2025-11-17-13-48-46
 'use strict';function initMainMenuGuide(){$('[data-toggle\x3d"popover"]').popover({delay:{show:500,hide:100},trigger:"hover"})};

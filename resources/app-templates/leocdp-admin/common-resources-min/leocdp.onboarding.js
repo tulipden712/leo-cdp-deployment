8ce7ca8237f9258eeecbf9fc52e087cdbe749e3f
trieu <tantrieuf31.database@gmail.com>
2025-11-19 18:20:32 +0700

    segment event statistics in profile

--- resources/app-templates/leocdp-admin/common-resources-min/leocdp.onboarding.js
+++ resources/app-templates/leocdp-admin/common-resources-min/leocdp.onboarding.js
@@ -1,2 +1,2 @@
-// Version: v_0.9.0_2025-11-17-13-48-46
+// Version: v_0.9.0_2025-11-19-18-19-34
 'use strict';function initMainMenuGuide(){$('[data-toggle\x3d"popover"]').popover({delay:{show:500,hide:100},trigger:"hover"})};

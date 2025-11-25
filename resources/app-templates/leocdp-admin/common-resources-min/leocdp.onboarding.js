319155dd6c4c6eee86861a49cdaff329e7a8f555
trieu <tantrieuf31.database@gmail.com>
2025-11-25 16:08:20 +0700

    upgrade for keycloak SSO and public API of CDP

--- resources/app-templates/leocdp-admin/common-resources-min/leocdp.onboarding.js
+++ resources/app-templates/leocdp-admin/common-resources-min/leocdp.onboarding.js
@@ -1,2 +1,2 @@
-// Version: v_0.9.0_2025-11-19-18-19-34
+// Version: v_0.9.0_2025-11-25-16-06-43
 'use strict';function initMainMenuGuide(){$('[data-toggle\x3d"popover"]').popover({delay:{show:500,hide:100},trigger:"hover"})};

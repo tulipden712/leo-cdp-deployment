aed9686acef69e862945c4f493561e4977a8d50a
trieu <tantrieuf31.database@gmail.com>
2025-12-02 14:20:06 +0700

    upgrade system, fix bugs

--- resources/app-templates/leocdp-admin/common-resources-min/leocdp.onboarding.js
+++ resources/app-templates/leocdp-admin/common-resources-min/leocdp.onboarding.js
@@ -1,2 +1,2 @@
-// Version: v_0.9.0_2025-12-01-18-25-08
+// Version: v_0.9.0_2025-12-02-14-15-07
 'use strict';function initMainMenuGuide(){$('[data-toggle\x3d"popover"]').popover({delay:{show:500,hide:100},trigger:"hover"})};

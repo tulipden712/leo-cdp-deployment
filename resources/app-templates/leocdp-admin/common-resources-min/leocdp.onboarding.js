fba259ffbde8efa2d4a4c1447920c4fd74996e98
trieu <tantrieuf31.database@gmail.com>
2025-12-12 14:48:00 +0700

    update for sending multi events in batch

--- resources/app-templates/leocdp-admin/common-resources-min/leocdp.onboarding.js
+++ resources/app-templates/leocdp-admin/common-resources-min/leocdp.onboarding.js
@@ -1,2 +1,2 @@
-// Version: v_0.9.0_2025-12-02-14-15-07
+// Version: v_0.9.0_2025-12-12-12-24-53
 'use strict';function initMainMenuGuide(){$('[data-toggle\x3d"popover"]').popover({delay:{show:500,hide:100},trigger:"hover"})};

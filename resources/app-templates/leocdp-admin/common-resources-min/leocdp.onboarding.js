46dcedba3b8e7b3d85605c49bd2d4d2f57463699
trieu <tantrieuf31.database@gmail.com>
2026-02-25 12:59:05 +0700

    improve UI UX

--- resources/app-templates/leocdp-admin/common-resources-min/leocdp.onboarding.js
+++ resources/app-templates/leocdp-admin/common-resources-min/leocdp.onboarding.js
@@ -1,2 +1,2 @@
-// Version: v_0.9.0_2026-01-09-17-45-32
+// Version: v_0.9.0_2026-02-25-12-58-33
 'use strict';function initMainMenuGuide(){$('[data-toggle\x3d"popover"]').popover({delay:{show:500,hide:100},trigger:"hover"})};

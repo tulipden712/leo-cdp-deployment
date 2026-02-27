be3b465d70863761b242bafc0dc45e290a94812c
trieu <tantrieuf31.database@gmail.com>
2026-02-27 15:51:08 +0700

    add help menu item, improve campaign editor

--- resources/app-templates/leocdp-admin/common-resources-min/leocdp.onboarding.js
+++ resources/app-templates/leocdp-admin/common-resources-min/leocdp.onboarding.js
@@ -1,2 +1,2 @@
-// Version: v_0.9.0_2026-02-25-12-58-33
+// Version: v_0.9.0_2026-02-27-15-50-11
 'use strict';function initMainMenuGuide(){$('[data-toggle\x3d"popover"]').popover({delay:{show:500,hide:100},trigger:"hover"})};

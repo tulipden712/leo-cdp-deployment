c1be945107a76fbdfda18787a09af79c660e328d
trieu <tantrieuf31.database@gmail.com>
2026-04-03 11:29:21 +0700

    fix bugs update profile with feedback data

--- resources/app-templates/leocdp-admin/common-resources-min/leocdp.onboarding.js
+++ resources/app-templates/leocdp-admin/common-resources-min/leocdp.onboarding.js
@@ -1,2 +1,2 @@
-// Version: v_0.9.0_2026-02-27-15-50-11
+// Version: v_0.9.0_2026-04-03-11-28-21
 'use strict';function initMainMenuGuide(){$('[data-toggle\x3d"popover"]').popover({delay:{show:500,hide:100},trigger:"hover"})};

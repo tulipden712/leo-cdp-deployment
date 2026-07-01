44d567bec0e64067e9a7d4763d2b906d08028ca1
trieu <tantrieuf31.database@gmail.com>
2026-07-01 10:10:47 +0700

    update code

--- resources/app-templates/leocdp-admin/common-resources-min/leocdp.onboarding.js
+++ resources/app-templates/leocdp-admin/common-resources-min/leocdp.onboarding.js
@@ -1,2 +1,2 @@
-// Version: v_0.9.0_2026-06-12-19-06-55
+// Version: v_0.9.0_2026-07-01-09-36-30
 'use strict';function initMainMenuGuide(){$('[data-toggle\x3d"popover"]').popover({delay:{show:500,hide:100},trigger:"hover"})};
